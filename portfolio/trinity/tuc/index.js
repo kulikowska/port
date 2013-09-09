ab2c0e695d0ad7fdda8f47f0d1bd663d5d967e31
Tuc <jarek707@gmail.com>
2013-09-09 16:53:38 -0400

    Finished writeup on objects and arrays in portfolio/trinigy/index.js

--- portfolio/trinity/tuc/index.js
+++ portfolio/trinity/tuc/index.js
@@ -27,7 +27,13 @@ function getRating() {
         return localStorage['ratingValue'];
     }
 }
-
+//=====================================================================================================
+//
+//
+//      OBJECTS AND ARRAYS for later
+//
+//
+//=====================================================================================================
 // Objects and arrays
 // Create empty object
 var myObject = {};
@@ -47,16 +53,27 @@ var first = myObject.first;
 // or
 var last = myObject['last'];
 
-console.log( first + ' ' + last );
+console.log( 'First and last: ' + first + ' ' + last );
+// or
+console.log( 'First and last retrieved directly: ' + myObject['first'] + ' ' + myObject.last );
 
-// You can also create the same object on one line like this:
+// You can also create an object on one line like this:
 myObject = {'first' : 'Jo', 'last' : 'Blo', 'phone' : '555-555-5555', 'email' : 'jo@blo.com'};
+
 console.log( 'Phone is: ' + myObject.phone + ' and email is: ' + myObject['email']);
 
+// Changing values of an object
+myObject.email = 'jo@gmail.com';
+myObject['phone'] = '123-456-7890';
+
+console.log( 'After change Phone is: ' + myObject.phone + ' and email is: ' + myObject['email']);
 
 //Objects allow you to reference their fields by name like 'email' or 'first'
 //In arrays you access elements by order (or position)
 
+console.log( '-' );
+console.log( 'ARRAYS' );
+console.log( '-' );
 // Empty array
 var myArray = [];
 
@@ -67,6 +84,8 @@ console.log( 'Name is: ' + myArray[0] + ' and phone is: ' + myArray[2]);
 
 //you can change a value of an array like this :
 myArray[0] = 'Bill';
+myArray[2] = '123-456-7890';
+
 console.log( 'After chage name is: ' + myArray[0] + ' and phone is: ' + myArray[2]);
 
 //
@@ -74,10 +93,27 @@ console.log( 'After chage name is: ' + myArray[0] + ' and phone is: ' + myArray[
 //
 
 // You can use objects inside arrays and arrays inside objects
+console.log( '-');
+console.log( 'OBJECT OF ARRAYS');
+console.log( '-');
+
 var bigObject = { 'like' : ['blue','green'], 'nolike' : ['brown','pink']};
 
 console.log( 'Like them all: ' + bigObject.like );
 console.log( 'Sure like this one: ' + bigObject.like[0] );
+console.log( 'Fucking hate this one: ' + bigObject.nolike[0] );
+console.log( 'Not crazy about this one: ' + bigObject.nolike[1] );
 
 // Now objects inside arrays
-var bigArray = [{'name' : 'Jo'},{}];
+console.log( '-');
+console.log( 'ARRAY OF OBJECTS');
+console.log( '-');
+
+var bigArray = [{'name' : 'Jo'},{'name' : 'Bill', 'email' : 'bill@nothing.com'}];
+
+console.log( bigArray[1]['email']);
+console.log( bigArray[0].name);
+//
+// That's about all for objects and arrays
+// It takes a while to get it under your skin, again, practice/play with it is the only way
+//
