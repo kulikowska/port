738f762f832b3fc150975a653555a90d2f11e3ce
Tuc <jarek707@gmail.com>
2013-09-09 16:23:25 -0400

    quick push

--- portfolio/trinity/trinity.js
+++ portfolio/trinity/trinity.js
@@ -2,14 +2,18 @@
 		var currentRating = $('.rate-points .number').text();
 		var intRating = parseInt(currentRating);
 		intRating += 1;
-			 if (intRating > 0) {
-			    intRating = '+' + intRating;}
-		$('.rate-points .number').text(+  intRating);
+		if (intRating > 0) {
+		    intRating = '+' + intRating;
+        }
+		$('.rate-points .number').text(intRating);
 	}
 
 	function subtractRating() {
 		var currentRating = $('.rate-points .number').text();
 		var intRating = parseInt(currentRating);
 		intRating -= 1;
-		$('.rate-points .number').text(+ intRating);
+		if (intRating > 0) {
+		    intRating = '+' + intRating;
+        }
+		$('.rate-points .number').text(intRating);
 	}
--- portfolio/trinity/tuc/index.js
+++ portfolio/trinity/tuc/index.js
@@ -33,3 +33,51 @@ function getRating() {
 var myObject = {};
 
 // Add values to the object
+// Method 1: object style
+myObject.first = 'Jo';
+console.log( myObject );
+
+// Method 2: array style (has the same effect)
+myObject['last'] = 'Blo';
+console.log( myObject );
+
+// This object has two fields named 'first' and 'last'
+// To get the value of a particular field you can do this
+var first = myObject.first;
+// or
+var last = myObject['last'];
+
+console.log( first + ' ' + last );
+
+// You can also create the same object on one line like this:
+myObject = {'first' : 'Jo', 'last' : 'Blo', 'phone' : '555-555-5555', 'email' : 'jo@blo.com'};
+console.log( 'Phone is: ' + myObject.phone + ' and email is: ' + myObject['email']);
+
+
+//Objects allow you to reference their fields by name like 'email' or 'first'
+//In arrays you access elements by order (or position)
+
+// Empty array
+var myArray = [];
+
+// or initialized array
+myArray = ['joe','blo','555-555-5555','joe@blo.com'];
+
+console.log( 'Name is: ' + myArray[0] + ' and phone is: ' + myArray[2]);
+
+//you can change a value of an array like this :
+myArray[0] = 'Bill';
+console.log( 'After chage name is: ' + myArray[0] + ' and phone is: ' + myArray[2]);
+
+//
+// Advanced use of arrays and objects
+//
+
+// You can use objects inside arrays and arrays inside objects
+var bigObject = { 'like' : ['blue','green'], 'nolike' : ['brown','pink']};
+
+console.log( 'Like them all: ' + bigObject.like );
+console.log( 'Sure like this one: ' + bigObject.like[0] );
+
+// Now objects inside arrays
+var bigArray = [{'name' : 'Jo'},{}];
