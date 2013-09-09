89a876d6570c42303cb12b451037a4d2e4dbca44
Ruby <rubykulikowska@gmail.com>
2013-09-09 16:21:19 -0400

    ...pushing...

--- portfolio/trinity/trinity.js
+++ portfolio/trinity/trinity.js
@@ -1,17 +1,15 @@
 	function addRating() {
 		var currentRating = $('.rate-points .number').text();
-		console.log( currentRating);
 		var intRating = parseInt(currentRating);
-		console.log( intRating );
 		intRating += 1;
+			 if (intRating > 0) {
+			    intRating = '+' + intRating;}
 		$('.rate-points .number').text(+  intRating);
 	}
 
 	function subtractRating() {
 		var currentRating = $('.rate-points .number').text();
-		console.log( currentRating);
 		var intRating = parseInt(currentRating);
-		console.log( intRating );
 		intRating -= 1;
 		$('.rate-points .number').text(+ intRating);
 	}
