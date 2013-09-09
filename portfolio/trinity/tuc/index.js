ddf4fa7cbd859ca464d6661ba9e4884a80a754d1
Tuc <jarek707@gmail.com>
2013-09-09 14:38:04 -0400

    Added dynamic number increase to rating

---
+++ portfolio/trinity/tuc/index.js
@@ -0,0 +1,8 @@
+function addRating() {
+    var currentRating = $('.rate-points .number').text();
+    console.log( currentRating);
+    var intRating = parseInt(currentRating);
+    console.log( intRating );
+    intRating += 1;
+    $('.rate-points .number').text('+' + intRating);
+}
