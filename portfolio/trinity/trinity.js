4892f11c03a1aef0cc2477dfb3a981bebe2ccbef
Ruby <rubykulikowska@gmail.com>
2013-09-16 14:55:27 -0400

    ...pushing...

--- portfolio/trinity/trinity.js
+++ portfolio/trinity/trinity.js
@@ -41,78 +41,10 @@
 		}
 	}
 
+	var tags = ['css', 'css2', 'css3', 'moreStuff'];
 
-	console.log ( 'Console log starts here' );
-	console.log ( ' ' );
-	console.log ( ' Objects ' );
-	console.log ( ' ' );
-	 
-	var myObject = {};
+	for (var i=0; i<tags.length; i++) 
 
-	myObject.hi = 'First one';
-	myObject['wassup'] = 'middle';
-	myObject.bye = 'Last one';
-	console.log( myObject );
-	 
-	var hi = myObject.hi;
-	var bye1 = myObject['bye'];
+	$('.tagstagstags').append('<li>' + tags[i] + '</li>');
 
-	console.log( 'First one and last one: ' + hi + ' ' + bye1);
-
-	console.log( 'First and last retrieved directly (not from the var): ' + myObject.hi + ' '  + myObject['wassup'] + ' ' + myObject.bye );
-
-	myObject = {'first' : 'Jo' , 'last' : 'Blo', 'phone' : '555-555-5555' , 'email' : 'jo@blo.com'};
-
-	console.log( 'Phone is: ' + myObject.phone + ' and email is ' + myObject['email']);
-
-	myObject.email = 'new@email.com';
-	myObject['phone'] = '123-456-7890';
-
-	console.log( ' After changing the values phone is: ' + myObject.phone + ' and e-mail is: ' + myObject['email']);
-
-	console.log( ' ' );
-	console.log( ' Arrays ' );
-	console.log( ' ' );
-
-	var myArray = [];
-
-	myArray = ['joe', 'blo', '555-555-5555', 'joe@blo.com'];
-
-	console.log( 'The whole array is: ' + JSON.stringify(myArray));
-	console.log( 'Name is: ' + myArray[0] + ',  phone is ' + myArray[2] + ', email is ' + myArray[3]);
-
-	myArray[0] = 'Joe 2';
-	myArray[2] = '123-345-4325';
-
-	console.log( 'After change name is: ' + myArray[0] + ', and phone is ' + myArray[2]);
-	 
-	console.log ( ' ' );
-	console.log ( 'Object of arrays');
-	console.log ( ' ' );
-
-	var	 bigObject = { 'strong' : ['pitties', 'rotties'], 'fast' : ['huskies', 'greyhounds']};
-
-	console.log( 'The whole object is this: ' +JSON.stringify(bigObject) );
-	console.log( 'Strong dogs: ' + bigObject.strong );
-	console.log( 'But this one is slow: ' + bigObject.strong[1] );
-	console.log( 'Fast dog: ' + bigObject.fast[0] );
-	console.log( 'Even faster dog: ' + bigObject.fast[1] );
 	
-	console.log( ' ' );
-	console.log( 'Array of Objects');
-	console.log( ' ' );
-
-	var bigArray = [{'first' : 'Jo', 'last' : 'Blo'},{'name' : 'Bill', 'email' : 'bill@nothing.com'}];
-
-	console.log( 'The whole array is this: ' +JSON.stringify(bigArray) );
-	console.log( 'Email field of the second array object: ' + bigArray[1]['email']);
-	console.log( 'First field of the first array object: ' + bigArray[0].first);
-	console.log( 'Last field of the first array object: ' + bigArray[0]['last']);
-
-	bigArray[0]['last'] = 'new value';
-	
-	console.log( 'After change of the last field of the first array object: ' + bigArray[0]['last']);
-	console.log( 'The whole array after change: ' + JSON.stringify(bigArray) );
-
-	// Cool, I'm getting the swing of it. I definitely get everything in theory, 
-	// but I'll mess around with it some more to really get a feel for it. 
