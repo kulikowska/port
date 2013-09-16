d9fe932f4d6f2a730fe8aad481607a96401c3dc1
Ruby <rubykulikowska@gmail.com>
2013-09-16 16:10:32 -0400

    ...pushing...

--- portfolio/trinity/trinity.js
+++ portfolio/trinity/trinity.js
@@ -2,12 +2,27 @@
 		$('.rate-points .number').text('+' + getRating());
 
 		var tags = [ {"name" : "css", "href" : "http://www.shidokanmontreal.ca/"}, 
-                     {"name" : "css2"}, 
-                     {"name" : "css3"}, 
-                     {"name" : "moreStuff"}
+                     {"name" : "css2", "href" : "http://www.icondeposit.com/system:page-tags/tag/css2" }, 
+                     {"name" : "css3", "href" : "http://www.icondeposit.com/system:page-tags/tag/css3"}, 
+                     {"name" : "design", "href" : "http://www.icondeposit.com/system:page-tags/tag/css3"},
+					 {"name" : "fancy-box", "href" : "http://www.icondeposit.com/system:page-tags/tag/fancy-box"},
+					 {"name" : "free", "href" : "http://www.icondeposit.com/system:page-tags/tag/fancy-box"},
+					 {"name" : "freebie", "href" : "http://www.icondeposit.com/system:page-tags/tag/free"},
+					 {"name" : "html", "href" : "http://www.icondeposit.com/system:page-tags/tag/free"},
+					 {"name" : "html5", "href" : "http://www.icondeposit.com/system:page-tags/tag/free"},
+					 {"name" : "images", "href" : "http://www.icondeposit.com/system:page-tags/tag/images"},
+					 {"name" : "jquery", "href" : "http://www.icondeposit.com/system:page-tags/tag/jquery"},
+					 {"name" : "nivo-slider", "href" : "http://www.icondeposit.com/system:page-tags/tag/nevo-slider"},
+					 {"name" : "quality", "href" : "http://www.icondeposit.com/system:page-tags/tag/quality"},
+					 {"name" : "resource", "href" : "http://www.icondeposit.com/system:page-tags/tag/resource"},
+					 {"name" : "slider", "href" : "http://www.icondeposit.com/system:page-tags/tag/slider"},
+					 {"name" : "template", "href" : "http://www.icondeposit.com/system:page-tags/tag/template"},
+					 {"name" : "theme", "href" : "http://www.icondeposit.com/system:page-tags/tag/theme"}
+
+
         ];
 		for (var i=0; i<tags.length; i++){
-			$('.tagstagstags').append('<a href="' + tags[i].href + '">' + tags[i].name + '</a>');
+			$('.tagstagstags').append('<a href="' + tags[i].href + '">' + tags[i].name + '</a>' + ' ' );
 	    }	
     });
 
