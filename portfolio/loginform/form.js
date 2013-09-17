ffaf16693b91d9cdd0014f74513dd90944502264
Tuc <jarek707@gmail.com>
2013-09-17 16:26:37 -0400

    quick push

--- portfolio/loginform/form.js
+++ portfolio/loginform/form.js
@@ -1,8 +1,17 @@
 dataObj = {};
 
 function saveform(){
+/*
     dataObj.first = $('form [name="first"]').val();
-    dataObj.last= $('form .last').val();
+    dataObj.last  = $('form .last').val();
     dataObj.email = $('form #email').val();
+*/
+
+    dataObj = {
+        "first" : $('form [name="first"]').val(),
+        "last"  : $('form .last').val(),
+        "email" : $('form #email').val()
+    };
+
 	console.log( dataObj );
 }
