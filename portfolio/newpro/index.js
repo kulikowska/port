4699a6d67a934f211bb6b10e242b6a8bfe05a9dd
Tuc <jarek707@gmail.com>
2013-09-26 15:11:54 -0400

    quick push

--- portfolio/newpro/index.js
+++ portfolio/newpro/index.js
@@ -1,16 +1,11 @@
+var list = [    {"position" : "Guitarist", "name" : "Robbie", "last" : "Robertson", "email" : "Robbie@band.com"},
+	            {"position" : "Pianist", "name" : "Richard", "last" : "Emmanuel", "email" : "Richard@band.com"},
+			    {"position" : "Drummer", "name" : "Levon", "last" : "Helm", "email" : "Levon@band.com"},
+			    {"position" : "Bassist", "name" : "Rick", "last" : "Danko", "email" : "Rick@band.com"},
+			    {"position" : "Organist", "name" : "Garth", "last" : "Hudson", "email" : "Garth@band.com"}
+]; 
+
 $(document).ready( function() {
-	
-	var list = [ {"position" : "Guitarist", "name" : "Robbie", "last" : "Robertson", "email" : "Robbie@band.com"},
-				 {"position" : "Pianist", "name" : "Richard", "last" : "Emmanuel", "email" : "Richard@band.com"},
-				 {"position" : "Drummer", "name" : "Levon", "last" : "Helm", "email" : "Levon@band.com"},
-				 {"position" : "Bassist", "name" : "Rick", "last" : "Danko", "email" : "Rick@band.com"},
-				 {"position" : "Organist", "name" : "Garth", "last" : "Hudson", "email" : "Garth@band.com"}
-    ]; 
-
-    for (var i=0; i<list.length; i++){
-        $('.content').append( "<li>" +  list[i].name + list[i].last + list[i].email + "</li>" ); 
-    }
-
     for (var i=0; i<list.length; i++){
         $('.menu').append("<li>" +  list[i].position + "</li> ");
     }
