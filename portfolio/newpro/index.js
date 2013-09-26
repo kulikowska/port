273445b17def30a649660b7d0dae5fb9cc8476ec
Ruby <rubykulikowska@gmail.com>
2013-09-26 14:40:04 -0400

    ...pushing...

--- portfolio/newpro/index.js
+++ portfolio/newpro/index.js
@@ -1,14 +1,18 @@
 $(document).ready( function() {
 	
-	var list = [ {"name" : "Robbie", "last" : "Robertson", "email" : "Robbie@band.com"};
-				 {"name" : "Richard", "last" : "Emmanuel", "email" : "Richard@band.com"};
-				 {"name" : "Levon", "last" : "Helm", "email" : "Levon@band.com"};
-				 {"name" : "Rick", "last" : "Danko", "email" : "Rick@band.com"};
-				 {"name" : "Garth", "last" : "Hudson", "email" : "Garth@band.com"}
+	var list = [ {"position" : "Guitarist", "name" : "Robbie", "last" : "Robertson", "email" : "Robbie@band.com"},
+				 {"position" : "Pianist", "name" : "Richard", "last" : "Emmanuel", "email" : "Richard@band.com"},
+				 {"position" : "Drummer", "name" : "Levon", "last" : "Helm", "email" : "Levon@band.com"},
+				 {"position" : "Bassist", "name" : "Rick", "last" : "Danko", "email" : "Rick@band.com"},
+				 {"position" : "Organist", "name" : "Garth", "last" : "Hudson", "email" : "Garth@band.com"}
 
 		]; 
 
 		for (var i=0; i<list.length; i++){
-			$('.menu').append( tags[i].name + tags[i].last + tags[i].email); 
+			$('.content').append( "<li>" +  list[i].name + list[i].last + list[i].email + "</li>" ); 
+		}
+
+		for (var i=0; i<list.length; i++){
+			$('.menu').append("<li>" +  list[i].position + "</li> ");
 		}
 	});
