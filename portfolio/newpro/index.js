8987fb4749ae5a3ca32cf57a87ff5d6474da7599
Tuc <jarek707@gmail.com>
2013-09-27 14:21:34 -0400

    quick push

--- portfolio/newpro/index.js
+++ portfolio/newpro/index.js
@@ -7,6 +7,13 @@ var list = [    {"position" : "Guitarist", "name" : "Robbie", "last" : "Robertso
 			    {"position" : "Organist", "name" : "Garth", "last" : "Hudson", "email" : "Garth@band.com"}
 ]; 
 
+var listO = { 'a' : {"position" : "Guitarist", "name" : "Robbie", "last" : "Robertson", "email" : "Robbie@band.com"},
+	          'b' : {"position" : "Pianist", "name" : "Richard", "last" : "Emmanuel", "email" : "Richard@band.com"},
+			  'c' : {"position" : "Drummer", "name" : "Levon", "last" : "Helm", "email" : "Levon@band.com"},
+			  'd' : {"position" : "Bassist", "name" : "Rick", "last" : "Danko", "email" : "Rick@band.com"},
+			  'e' : {"position" : "Organist", "name" : "Garth", "last" : "Hudson", "email" : "Garth@band.com"}
+}; 
+
 $(document).ready( function() {
     for (var i=0; i<list.length; i++){
         $('.menu').append("<li onClick='musicianInfo(" + i + ")'>" +  list[i].position + "</li> ");
