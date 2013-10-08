aca7586be94bc21ff9a9d82a5b7578510fae9ac1
Tuc <jarek707@gmail.com>
2013-10-08 16:43:17 -0400

    quick push

--- portfolio/cal/index.js
+++ portfolio/cal/index.js
@@ -7,8 +7,8 @@ var Actions = {
     },
     'removeTask' : function() {
 		var allLi = $(this.container).find('li');
-        console.log( allLi);
-        console.log($(allLi[i]) ) ;
+        console.log( allLi[0]);
+        console.log($(allLi[0]) ) ;
 
 		for (var i=0; i<allLi.length; i++){
 		    if ($(allLi[i]).find('[type="checkbox"]').is(':checked')){
