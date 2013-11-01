75e730abbc715c52717ec42644fcb1523b30de35
Ruby <rubykulikowska@gmail.com>
2013-11-01 16:50:21 -0400

    ...pushing...

---
+++ portfolio/jsstuff/index.js
@@ -0,0 +1,26 @@
+function makewhite(el) {
+	if ( $(el).html() == 'js fuckery' ) {
+		$(el).text('its white now');
+		$('.content').addClass('white').text('Im White Now!');
+	} else {
+		$(el).text('js fuckery');
+		$('.content').removeClass('white').text('stuff');
+		$('.content2').removeClass('black').text('next up');
+	}
+}
+
+function makeblack(el) {
+	if ( $(el).hasClass('white')) {
+		$(el).html('Im White Now! And that one is black!');
+		$('.content2').addClass('black').text('You Made Me Black!');
+	} else {
+		alert('click the one above me first');
+	}
+}
+
+function original(el) {
+	if ( $(el).hasClass('black') && $('.content').hasClass('white')) {
+		$(el).removeClass('black').text('next up');
+		$('.content').removeClass('white').text('stuff');
+	} 
+}
