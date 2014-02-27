5b8ac045b0622a26ebf1e4b3d32055add59572da
Ruby <rubykulikowska@gmail.com>
2014-02-27 15:34:38 -0500

    ...pushing...

---
+++ portfolio/slide/index.js
@@ -0,0 +1,56 @@
+$(document).ready(function() {
+
+	var currentPosition = 0;
+	var slideWidth = 700;
+	var slides = $('.slide');
+	var numberOfSlides = slides.length;
+	var slideShowInterval;
+	var speed = 10000;
+
+	//Timer
+	slideShowInterval = setInterval(changePosition, speed);
+
+	slides.wrapAll('<div id="slidesHolder"></div>')
+
+	slides.css({ 'float' : 'left' });
+	
+	$('#slidesHolder').css('width', slideWidth * numberOfSlides);
+
+	$('.slideshowWindow')
+		.append('<span class="nav" id="leftNav">Move Left</span>')
+		.append('<span class="nav" id="rightNav">Move Right</span>');
+
+	manageNav(currentPosition);
+
+	function changePosition() {
+		if(currentPosition == numberOfSlides - 1) {
+			currentPosition = 0;
+			manageNav(currentPosition); 
+		} else {
+			currentPosition++;
+			manageNav(currentPosition);
+		}
+		moveSlide();
+	}
+
+	function moveSlide() {
+		$('#slidesHolder').animate({'marginLeft' : slideWidth*(-currentPosition)});
+	}
+
+	function manageNav(position) {
+		if(position==0){ $('#leftNav').hide() }
+		else { $('#leftNav').show() }
+
+		if(position==numberOfSlides-1){ $('#rightNav').hide() }
+		else { $('#rightNav').show() }
+	}
+
+	$('.nav').bind('click', function() {
+		currentPosition = ($(this).attr('id')=='rightNav')
+		? currentPosition+1 : currentPosition-1;
+
+		manageNav(currentPosition);
+		clearInterval(slideShowInterval);
+		slideSHowInterval = setInterval(changePosition, speed);
+		moveSlide();
+});
