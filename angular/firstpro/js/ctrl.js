5ed8e727a0d886f5541b1c041f3f6654931930a7
Ruby <rubykulikowska@gmail.com>
2014-08-02 19:37:17 -0400

    ...pushing...

--- angular/firstpro/js/ctrl.js
+++ angular/firstpro/js/ctrl.js
@@ -1,28 +1,34 @@
 AP
 .controller('dad', function ($scope) {
 	$scope.datavar = 'some data';
-})
-
-.controller('mainass', function ($scope) {
 	$scope.topdiv = true;
 	$scope.toggle = function() {
 		$scope.topdiv = !$scope.topdiv;
-	}
+	};
+	$scope.divToggle = [true, false ,false];
+	$scope.changeTab = function(arg) {
+		for (var i=0; i<3; i++)
+			$scope.divToggle[i] = false;
+		$scope.divToggle[arg] = true;
+	};
+	$scope.list=[
+		{"first":"jo", "last":"blo", "email":"jo@blo"},
+		{"first":"bill", "last":"dude", "email":"bill@dude"},
+		{"first":"guy", "last":"williams", "email":"guy@williams"}
+	];
+	$scope.myclick=function(arg){
+		$scope.item=arg; 
+	};
+	$scope.showLeft=true;
+	$scope.switchit=function(){
+		$scope.showLeft = !$scope.showLeft;
+		};
+	
 })
+
 .controller('bigass', function ($scope) {
 	$scope.myvar="mystring"; 
-	$scope.list=[
-		{"first":"jo", "last":"blo"},
-		{"first":"bill", "last":"dude"},
-		{"first":"bill", "last":"dude"},
-		{"first":"bill", "last":"dude"},
-		{"first":"bill", "last":"dude"},
-		{"first":"bill", "last":"dude"}
-	];
 	$scope.bigassclass="red";
-	$scope.myclick=function(arg){
-		alert(arg);
-	};
 })
 .controller('justass', function ($scope) {
 	$scope.myvar="mystring"; 
@@ -32,7 +38,20 @@ AP
 	];
 	$scope.bigassclass="yellow";
 	$scope.myclick=function(arg){
-		alert(arg);
+		$scope.item=arg; 
 	};
+})
+.controller('puppyctrl', function ($scope) {
+	$scope.puppies = [
+		{"breed":"rottie", "cuteness":9},
+		{"breed":"pittie", "cuteness":8},
+		{"breed":"husky", "cuteness":8}
+	];
+})
+.filter('howcute', function() {
+  	return function(input) {
+  		if (input == 9) return 'really fucking cute';
+		else return 'almost really fucking cute';
+  	};
 })
 ;
