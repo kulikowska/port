7a8aa89ae1a4db5f537675fb2c4e5e66c2ef4c53
Ruby <rubykulikowska@gmail.com>
2014-08-15 15:54:33 -0400

    ...pushing...

---
+++ angular/secondpro/js/ctrl.js
@@ -0,0 +1,10 @@
+AP
+.controller('datahomie', function ($scope) {
+	$scope.list=[
+		{"id":"1", "first":"Susie", "last":"Wang", "email":"susie@kendo.com"},
+		{"id":"2", "first":"Tania", "last":"Delage", "email":"tania@kendo.com"},
+		{"id":"3", "first":"Akane", "last":"D'Orangeville", "email":"akane@kendo.com"},
+		{"id":"4", "first":"Melissa", "last":"Djap", "email":"melissa@kendo.com"},
+		{"id":"5", "first":"Martina", "last":"Solano", "email":"martina@kendo.com"}
+	];
+})
