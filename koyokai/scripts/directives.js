2263c5f6153b73d2e8373c04bfc845562e88e190
Ruby <rubykulikowska@gmail.com>
2014-11-04 20:41:30 -0500

    ...pushing...

--- koyokai/scripts/directives.js
+++ koyokai/scripts/directives.js
@@ -14,6 +14,22 @@ APP
         replace: false,
         templateUrl: 'html/dropdown.html',
         link: function($scope, $element, $attributes) {
+            $scope.menu = ['About', 'Gallery', 'Members', 'Kendo', 'Events'];
+            $scope.headerImg='headerimg';
+
+            $scope.shrinkAndSelect=function(value) {
+                $scope.headerImg='shrunk';
+                $scope.selected = value;
+            };
+
+            $scope.itemClass = function(value) {
+                return value === $scope.selected ? 'active' : undefined;
+            };
+
+            $scope.expandAndGo=function() {
+                $scope.headerImg='homeimg';
+                $scope.selected = '';
+                };
             }
         }
  })
