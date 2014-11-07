5603305d382def5d670bf586872dd3d3358d2fd7
jkulikowski <jarek707@gmail.com>
2014-11-07 18:23:42 -0500

    quick push

--- koyokai/scripts/directives.js
+++ koyokai/scripts/directives.js
@@ -43,12 +43,12 @@ APP
  .filter('toMenuText', function() {
     return function(input) {
         var ret;
-        switch(input+1) {
-            case 1: ret = "about"; break;
-            case 2: ret = "gallery"; break;
-            case 3: ret = "members"; break;
-            case 4: ret = "kendo"; break;
-            case 5: ret = "events"; break;
+        switch(input) {
+            case 0: ret = "about"; break;
+            case 1: ret = "gallery"; break;
+            case 2: ret = "members"; break;
+            case 3: ret = "kendo"; break;
+            case 4: ret = "events"; break;
             default : ret = 'out of range'; break;
        }
        return ret;
