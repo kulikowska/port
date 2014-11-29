e76f7f66708c06ac8afa0c9a66670e5725d7d20f
jkulikowski <jarek707@gmail.com>
2014-11-29 12:01:38 -0500

    quick push

--- maps/geo/js/mkTpls.sh
+++
@@ -1,9 +0,0 @@
-cat tpl.html |\
-    sed "s/<directive>/'/" |\
-    sed "s/<\/directive>/' :/" |\
-    sed "s/<html>/'/" |\
-    sed "s/<\/html>/',/"|\
-    tr -d '\n' |\
-    tr -s ' ' |\
-    sed "s/> </></g" |\
-    uglifyjs
