aff82085cfded1eaf1499d4f53f044b922b7ec5f
Ruby <rubykulikowska@gmail.com>
2015-01-16 13:50:34 -0500

    ...pushing...

---
+++ charts/index.php
@@ -0,0 +1,26 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+class DB {
+    public static $link;
+    public static function conn() {
+        self::$link = mysqli_connect("localhost","rubz","donkey","newdudes") or die("Error " . mysqli_error($link)); 
+    }
+
+    public static function getRows($sql) {
+        $ret = [];
+        $query = $sql or die("Error in the consult.." . mysqli_error(DB::$link)); 
+        $result = DB::$link->query($query); 
+        while($row = mysqli_fetch_array($result)) { 
+          array_push($ret, [$row['first'], $row['last']]);
+        } 
+        return $ret;
+    }
+}
+
+DB::conn();
+$rows = DB::getRows('SELECT * FROM members');
+foreach ($rows as $row) {
+    echo $row[0] . '...' . $row[1] . '<br /><br />';
+}
