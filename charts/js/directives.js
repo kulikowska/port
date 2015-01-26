2a8317dd574beb9ff0fea2401b21ee8898d7db2e
Ruby <rubykulikowska@gmail.com>
2015-01-26 15:42:31 -0500

    ...pushing...

--- charts/js/directives.js
+++ charts/js/directives.js
@@ -6,7 +6,13 @@ APP
         templateUrl: 'html/content.html',
        // template: TPL.content,
         link: function($scope, $element, $attributes) {
+            $scope.angularData = 'empty';
             google.load('visualization', '1.0', { callback : function() {}, packages: ['corechart', 'table'] });
+
+             $scope.getAngularData = function(data) {
+                $http.get('Controllers/index.php').success(data);
+                $scope.angularData = data;
+             }
          }
       }
 }])
