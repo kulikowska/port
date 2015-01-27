f3fa1f6d1a00f76aac6da18638a13f0a3a9946a4
Ruby <rubykulikowska@gmail.com>
2015-01-27 15:23:34 -0500

    ...pushing...

--- charts/js/directives.js
+++ charts/js/directives.js
@@ -7,11 +7,24 @@ APP
        // template: TPL.content,
         link: function($scope, $element, $attributes) {
             $scope.angularData = 'empty';
+            $scope.something = 'some random string';
             google.load('visualization', '1.0', { callback : function() {}, packages: ['corechart', 'table'] });
 
              $scope.getAngularData = function() {
-                $http.get('Controllers/index.php').success(function(data) { $scope.angularData = data;})
+                $http.get('Controllers/index.php').success(function(data) { 
+                    $scope.angularData = data;
+                })
              }
+
+             $scope.postAngularData = function(cb) {
+                $http({
+                    url: 'Controllers/index.php',
+                    method: 'POST',
+                    data: JSON.stringify($scope.something),
+                }).
+                success(cb);
+                alert($scope.something);
+            }
          }
       }
 }])
