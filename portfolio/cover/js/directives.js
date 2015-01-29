c34644130257638c1f97d9bb85b88376af0ffd5d
Ruby <rubykulikowska@gmail.com>
2015-01-29 12:49:18 -0500

    ...pushing...

--- portfolio/cover/js/directives.js
+++ portfolio/cover/js/directives.js
@@ -16,6 +16,56 @@ APP
         //templateUrl: 'html/header.html',
         template: TPL.canvasTest,
         link: function($scope, $element, $attributes) {
+            var canvas = document.getElementById('testCanvas');
+
+            var requestAnimationFrame = 
+                window.requestAnimationFrame ||
+                window.webkitRequestAnimationFrame ||
+                window.oRequestAnimateFrame ||
+                function(callback) {
+                    return setTimeout(callback, 1);
+                };
+
+            if (canvas.getContext) {
+                var context = canvas.getContext('2d');
+
+                var square = {
+                    'x': 50,
+                    'y': 50,
+                    'width': 100,
+                    'height': 100,
+                    'fill': '#fff'
+                };
+
+                var render = function() {
+                    context.clearRect(0, 0, canvas.width, canvas.height);
+
+                    context.beginPath();
+                    context.rect(50, 50, 100, 100);
+                    context.fillStyle = '#FFF';
+                    context.fill();
+
+                    requestAnimationFrame(render);
+                };
+                render();
+
+                var animate = function(prop, val, duration) {
+                    var start = new Date().getTime();
+                    var end = start + duration;
+                    var current = square[prop];
+                    var distance = val - current;
+
+                    var step = function() {
+                        var timestamp = new Date().getTime();
+                        var progress = Math.min((duration - (end - timestamp)) / duration, 1);
+
+                        square[prop] = current + (distance * progress);
+                        if (progress < 1) requestAnimationFrame(step);
+                    };
+                    return step();
+                };
+                animate('x', 0, 1000);
             }
         }
+    }
  }])
