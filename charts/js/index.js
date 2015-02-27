0748606372261780c167f9df94914b70050df21b
Ruby <rubykulikowska@gmail.com>
2015-02-26 20:32:00 -0500

    ...pushing...

--- charts/js/index.js
+++ charts/js/index.js
@@ -104,3 +104,63 @@ function getData() {
        } 
     });
 }
+
+var gl;
+
+function initWebGL(canvas) {
+  gl = null;
+    
+    try {
+        //Try to grab the standard context. If it fails, fallback to experimental.
+        gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
+    }
+    catch(e) {}
+
+    // If we don't have a GL context, give up now
+    if (!gl) {
+        alert("Unable to initialize WebGL. Your browser may not support it.");
+        gl = null;
+    }
+
+    return gl;
+}
+
+var camera, scene, renderer;
+var geometry, material, mesh;
+
+init();
+animate();
+
+function init() {
+
+camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
+camera.position.z = 1000;
+
+scene = new THREE.Scene();
+
+geometry = new THREE.BoxGeometry(200, 200, 200);
+material = new THREE.MeshBasicMaterial({
+color: 0xff0000,
+wireframe: true
+});
+
+mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer.domElement);
+
+}
+
+function animate() {
+
+requestAnimationFrame(animate);
+
+mesh.rotation.x += 0.01;
+mesh.rotation.y += 0.02;
+
+renderer.render(scene, camera);
+
+}
