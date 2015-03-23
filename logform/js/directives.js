cbfec57e5c7ff8c89ebc7a8e13272eacad854ab4
Ruby <rubykulikowska@gmail.com>
2015-03-23 19:33:32 -0400

    ...pushing...

--- logform/js/directives.js
+++ logform/js/directives.js
@@ -1,20 +1,20 @@
 APP
-//.config(['$httpProvider',
-//    function($httpProvider) {
-//        $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
-//        $httpProvider.defaults.headers.post['Content-Type']       = 'application/x-www-form-urlencoded; charset=UTF-8';
-//        $httpProvider.interceptors.push(['$q', function($q) {
-//            return {
-//                request: function(config) {
-//                    if (config.data && typeof config.data === 'object') {
-//                        config.data = $.param(config.data);
-//                    }
-//                    return config || $q.when(config);
-//                }
-//            };
-//        }]);
-//    }
-//])
+.config(['$httpProvider',
+    function($httpProvider) {
+        $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
+        $httpProvider.defaults.headers.post['Content-Type']       = 'application/x-www-form-urlencoded; charset=UTF-8';
+        $httpProvider.interceptors.push(['$q', function($q) {
+            return {
+                request: function(config) {
+                    if (config.data && typeof config.data === 'object') {
+                        config.data = $.param(config.data);
+                    }
+                    return config || $q.when(config);
+                }
+            };
+        }]);
+    }
+])
 .directive('content', ['$http', function($http) {
     return {
         restrict: 'C',
