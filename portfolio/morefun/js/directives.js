5032fbd77cc26631b14a8acab49635293ffe6430
Ruby <rubykulikowska@gmail.com>
2015-04-10 21:43:21 -0400

    ...pushing...

---
+++ portfolio/morefun/js/directives.js
@@ -0,0 +1,33 @@
+APP
+.directive('mainContent', [function() {
+    return {
+        restrict: 'C',
+        replace: false,
+        templateUrl: 'html/content.html',
+        //template: TPL.content,
+        link: function($scope, $element, $attributes) {
+            $scope.tabIdx = 1;
+         }
+      } 
+ }])
+.directive('projects', [function() {
+    return {
+        restrict: 'C',
+        replace: false,
+        templateUrl: 'html/projects.html',
+        //template: TPL.portfolio,
+        link: function($scope, $element, $attributes) {
+         }
+      } 
+ }])
+.directive('contact', [function() {
+    return {
+        restrict: 'C',
+        replace: false,
+        templateUrl: 'html/contact.html',
+        //template: TPL.portfolio,
+        link: function($scope, $element, $attributes) {
+         }
+      } 
+ }])
+ 
