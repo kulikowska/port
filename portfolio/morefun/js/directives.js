22d3e147fd13c767cd806469d3fa0c31d9382941
Ruby <rubykulikowska@gmail.com>
2015-04-12 13:56:40 -0400

    ...pushing...

--- portfolio/morefun/js/directives.js
+++ portfolio/morefun/js/directives.js
@@ -7,15 +7,22 @@ APP
         //template: TPL.content,
         link: function($scope, $element, $attributes) {
             $scope.middleDiv = 'middleGround';
+            $scope.smaller = 'smaller';
+            $scope.smallest = 'smallest';
 
             $scope.showPort = function() {
                 if (!$scope.portfolio) {
                     $scope.portfolio = true;
                     $scope.middleDiv = 'shrunkMiddle';
+                    //$scope.hideEm = 'hidden';
+                    $scope.smaller = 'evenSmaller';
+                    $scope.smallest = 'evenSmallest';
+                    $scope.mainUl = 'shrunkUl';
                 }
                 else {
                     $scope.portfolio = false;
                     $scope.middleDiv = 'middleGround';
+                    $scope.hideEm = '';
                 }
             }
          }
