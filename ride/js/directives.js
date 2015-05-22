2757af112218c682e6d87ca852472f89e92a9e59
Ruby <rubykulikowska@gmail.com>
2015-05-23 01:01:59 +0200

    ...pushing...

--- ride/js/directives.js
+++ ride/js/directives.js
@@ -9,37 +9,14 @@ APP
          }
       } 
  }])
-.directive('middleContainer', [function() {
+.directive('findRides', [function() {
     return {
         restrict: 'C',
         replace: false,
-        templateUrl: 'html/middle.html',
+        templateUrl: 'html/findRides.html',
         //template: TPL.portfolio,
         link: function($scope, $element, $attributes) {
-            $scope.middleDiv = 'middleGround';
-            $scope.smaller = 'smaller';
-            $scope.smallest = 'smallest';
-            $scope.imageContainer = 'imageContainer';
-
-            $scope.showPort = function() {
-                if (!$scope.portfolio) {
-                    $scope.portfolio = true;
-                    $scope.imageContainer = 'shrunkImageContainer';
-                    $scope.middleDiv = 'shrunkMiddle';
-                    $scope.smaller = 'evenSmaller';
-                    $scope.smallest = 'evenSmallest';
-                    $scope.mainUl = 'shrunkUl';
-                }
-                else {
-                    $scope.portfolio = false;
-                    $scope.middleDiv = 'middleGround';
-                    $scope.imageContainer = 'imageContainer';
-                    $scope.smaller = 'smaller';
-                    $scope.smallest = 'smallest';
-                    $scope.mainUl = '';
-                }
-            }
-         }
+        }
       } 
  }])
 .directive('portfolio', [function() {
