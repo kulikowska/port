ab82738ade3493fe06d4e6a1ebf0353b7b40be3d
Ruby <rubykulikowska@gmail.com>
2015-05-24 00:25:52 +0200

    ...pushing...

--- ride/js/directives.js
+++ ride/js/directives.js
@@ -20,38 +20,6 @@ APP
         }
       } 
  }])
-.directive('portfolio', [function() {
-    return {
-        restrict: 'C',
-        replace: false,
-        templateUrl: 'html/portfolio.html',
-        //template: TPL.portfolio,
-        link: function($scope, $element, $attributes) {
-         }
-      } 
- }])
-.directive('footerContainer', [function() {
-    return {
-        restrict: 'C',
-        replace: false,
-        templateUrl: 'html/footer.html',
-        //template: TPL.portfolio,
-        link: function($scope, $element, $attributes) {
-            $scope.footerClass = 'footer';
-
-            $scope.showHideFooter = function() {
-                if (!$scope.noFooter) {
-                    $scope.footerClass = 'shrunkFooter';
-                    $scope.noFooter = true;
-                }
-                else {
-                    $scope.noFooter = false;
-                    $scope.footerClass = 'footer';
-                }
-            }
-         }
-      } 
- }])
 .filter('checkActive', function() {
     return function(input) {
         return input ? 'active' : '';
