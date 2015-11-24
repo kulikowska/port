0a3c4ee6478e887eebd3a28352e04ad2e15efd16
jkulikowski <jarek707@gmail.com>
2015-11-23 21:52:13 -0500

    added blog

---
+++ portfolio/blog/js/directives.js
@@ -0,0 +1,34 @@
+APP
+.directive('mainHeader', [function() {
+    return {
+        restrict: 'C',
+        replace: false,
+        templateUrl: 'html/content.html',
+        //template: TPL.content,
+        link: function($scope, $element, $attributes) {
+         }
+      } 
+ }])
+.directive('footerContainer', [function() {
+    return {
+        restrict: 'C',
+        replace: false,
+        templateUrl: 'html/footer.html',
+        //template: TPL.portfolio,
+        link: function($scope, $element, $attributes) {
+            $scope.footerClass = 'footer';
+
+            $scope.showHideFooter = function() {
+                if (!$scope.noFooter) {
+                    $scope.footerClass = 'shrunkFooter';
+                    $scope.noFooter = true;
+                }
+                else {
+                    $scope.noFooter = false;
+                    $scope.footerClass = 'footer';
+                }
+            }
+         }
+      } 
+ }])
+ 
