e09414641d6c6fb6235a2f037222353df40a4fac
Ruby Kulikowska <rubykulikowska@Rubys-MacBook-Pro.local>
2016-08-21 18:42:04 -0400

    added north2

---
+++ portfolio/north2/js/directives.js
@@ -0,0 +1,11 @@
+APP
+.directive('content', [function() {
+    return {
+        restrict: 'C',
+        replace: false,
+        templateUrl: 'html/content.html',
+        //template: TPL.content,
+        link: function($scope, $element, $attributes) {
+         }
+      } 
+ }])
